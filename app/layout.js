c40b2d186c69ffd573611ae7689926f264761f44
Tuhina Das <tuhinakdas@gmail.com>
2024-08-19 08:49:07 -0500

    initialized project

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html Lang="en">
+      <body>
+        <div className="main"><div className="gradient" /></div>
+
+        <main className="app">{children}</main>
+      </body>
     </html>
   );
 }
